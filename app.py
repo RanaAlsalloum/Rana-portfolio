cca850f0534dbe603b8aee71b1d19000da2b53b0
RanaAlsalloum <162427486+RanaAlsalloum@users.noreply.github.com>
2024-03-07 11:32:02 +0000

    page with style
    page with style

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app=Flask(__name__) # first flask app
 
 @app.route('/')
 def hello_world():
-  return "Hello, Rana!"
+  return render_template('home.html')
+  
 
 if __name__ == "__main__": 
   print("I'm inside the if now")
